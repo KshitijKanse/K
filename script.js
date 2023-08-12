c3b8fa1e01cead8ac3d92a3db449462df1f8e5ca
KshitijKanse <119565041+KshitijKanse@users.noreply.github.com>
2023-08-12 11:45:21 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: [ "Developer", "Stock Trader", "Designer","Blogger","Animator"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
